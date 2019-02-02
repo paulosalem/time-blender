20bbfd02d14ad003c7d923cfca878212f53182ca
Paulo Salem <paulosalem@paulosalem.com>
2019-02-02 20:20:22 -0200

    Bumps (patch) version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='time-blender',
-      version='0.1',
+      version='0.1.2',
       description='A compositional time series generator.',
       url='https://github.com/paulosalem/time-blender',
       author='Paulo Salem',
